f93af3854632926493652bec1ed7e6ddddab209a
Jakub Kucharski <sagan07@gmail.com>
2018-05-14 21:40:15 +0200

    Najnowsza wersja

--- LoginMainTest.py
+++ LoginMainTest.py
@@ -4,13 +4,18 @@ import unittest
 from selenium import webdriver
 
 
-class GoogleTest(unittest.TestCase):
+class Test(unittest.TestCase):
 
     def test_valid_login(self):
         self.driver = webdriver.Firefox(executable_path=r"D:\Python\Drivery\geckodriver.exe")
         self.driver.get('https://ncplusgo.pl')
         log_in = Start(self.driver)
         log_in.userLogin_valid()
+
+#  W celu zamkniecia przegladarki mozna dodac:
+
+    #  def tearDown(self):
+        #  self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
